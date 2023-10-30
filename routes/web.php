b7f00de1d367d54e595e09b59ce0e18a0f68741d
Mohammad Sulaiman <smensulaiman007@gmail.com>
2023-10-30 17:58:05 +0900

    Image Upload

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,8 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,3 +19,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
